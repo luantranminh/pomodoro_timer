29700ce796019eaeb80c25b90bfbb95deb4b1b5e
Phuc Le Dien <phucledien@gmail.com>
2017-08-03 19:03:33 +0700

    completed main functions

---
+++ js/pomodoro.js
@@ -0,0 +1,71 @@
+pomodoro = 1500;
+shortBreak = 300;
+longBreak = 600;
+count = 0
+x = null;
+
+$(document).ready(function() {
+  $('#pomodoro').click(function() { Pomodoro(); });
+  $('#shortbreak').click(function() { ShortBreak(); });
+  $('#longbreak').click(function() { LongBreak(); });
+});
+
+
+
+function Pomodoro() {
+
+  reset(pomodoro);
+  start(pomodoro);
+  $('#startbut').click(function() { start(pomodoro); });
+  $('#stopbut').click(function() { stop(); });
+  $('#resetbut').click(function() { reset(pomodoro); });
+}
+
+function ShortBreak() {
+  reset(shortBreak);
+  start(shortBreak);
+  $('#startbut').click(function() { start(shortBreak); });
+  $('#stopbut').click(function() { stop(); });
+  $('#resetbut').click(function() { reset(shortBreak); });
+}
+
+function LongBreak() {
+  reset(longBreak);
+  start(longBreak);
+  $('#startbut').click(function() { start(longBreak); });
+  $('#stopbut').click(function() { stop(); });
+  $('#resetbut').click(function() { reset(longbreak); });
+}
+function start(time) {
+  if(x != null) return;
+  count = count == 0? time: count;
+  x = setInterval(function() {
+    count -= 1;
+    var minute = pad(Math.floor(count/60), 2)
+    var second = pad(count%60, 2)
+    $('#time').html(minute + ':' + second);
+    if(count <= 0 ) {
+      clearInterval(x)
+    }
+  }, 1000);
+}
+
+function stop() {
+  clearInterval(x);
+  x = null;
+}
+
+function reset(time) {
+  stop();
+  count = time;
+  x = null;
+  var minute = pad(Math.floor(count/60), 2)
+  var second = pad(count%60, 2)
+  $('#time').html(minute + ':' + second);
+}
+
+function pad(n, width, z) {
+  z = z || '0';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
